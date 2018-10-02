366dbac195777ae5fb971cc6977b156413f38882
Ashwin Joy <ashwinparekkudiyil@gmail.com>
2018-10-02 13:56:14 +0530

    Add files via upload

---
+++ todo/todoapp/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+class TodoForm(forms.Form):
+    text = forms.CharField(max_length=50, widget=forms.TextInput(
+    attrs={
+        'class' : 'form-control',
+        'placeholder' : ' A Django Todo '
+
+    }))
+
+
+
+
+
+     # <input type="text" class="form-control" placeholder="A Django Todo" >
